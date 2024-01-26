744d09a4ff199630c71803b5313303f0fcb1378a
Piohi <box1234567b@gmail.com>
2024-01-26 08:35:55 +0300

    complete

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -10,33 +10,35 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet var changeColorButton: UIButton!
-    var numberOfLight: Int = 0
     @IBOutlet var redColorCircle: UIView!
     @IBOutlet var yellowColorCircle: UIView!
     @IBOutlet var greenColorCircle: UIView!
     @IBOutlet var boxOfLight: UIView!
+    var numberOfLight: Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setUpStartLights()
         changeColorButton.setTitle("START", for: .normal)
-
-        
-        // Do any additional setup after loading the view.
     }
+    
     func setUpStartLights() {
-        boxOfLight.layer.cornerRadius = boxOfLight.frame.width / 3
-        redColorCircle.layer.cornerRadius = 50
-        print(redColorCircle.frame.height, redColorCircle.frame.width)
-        print(redColorCircle.layer.cornerRadius)
-        yellowColorCircle.layer.cornerRadius = yellowColorCircle.bounds.height / 2
-        greenColorCircle.layer.cornerRadius = greenColorCircle.frame.height / 2
+        boxOfLight.layer.cornerRadius = boxOfLight.frame.width / 4
+        redColorCircle.frame.size.height = UIScreen.main.bounds.height / 6.67
+        redColorCircle.frame.size.width = redColorCircle.frame.size.height
+        redColorCircle.layer.cornerRadius = redColorCircle.frame.height / 2
         redColorCircle.alpha = 0.2
+        yellowColorCircle.frame.size.height = UIScreen.main.bounds.height / 6.67
+        yellowColorCircle.frame.size.width = yellowColorCircle.frame.size.height
+        yellowColorCircle.layer.cornerRadius = yellowColorCircle.frame.height / 2
         yellowColorCircle.alpha = 0.2
+        greenColorCircle.frame.size.height = UIScreen.main.bounds.height / 6.67
+        greenColorCircle.frame.size.width = greenColorCircle.frame.size.height
+        greenColorCircle.layer.cornerRadius = greenColorCircle.frame.height / 2
         greenColorCircle.alpha = 0.2
     }
     
-    @IBAction func buttonPresed2(_ sender: UIButton) {
+    @IBAction func buttonPresed(_ sender: UIButton) {
         changeColorButton.setTitle("NEXT", for: .normal)
         if numberOfLight == 3 {
             numberOfLight = 0
@@ -63,34 +65,6 @@ class ViewController: UIViewController {
             redColorCircle.alpha = 0.2
         }
     }
-//    @objc func buttonPresed() {
-//        changeColorButton.setTitle("NEXT", for: .normal)
-//        if numberOfLight == 3 {
-//            numberOfLight = 0
-//        }
-//        switch numberOfLight {
-//        case 0: 
-//            redColorCircle.alpha = 1
-//            yellowColorCircle.alpha = 0.2
-//            greenColorCircle.alpha = 0.2
-//            numberOfLight += 1
-//        case 1:
-//            redColorCircle.alpha = 0.2
-//            yellowColorCircle.alpha = 1
-//            greenColorCircle.alpha = 0.2
-//            numberOfLight += 1
-//        case 2:
-//            redColorCircle.alpha = 0.2
-//            yellowColorCircle.alpha = 0.2
-//            greenColorCircle.alpha = 1
-//            numberOfLight += 1
-//        default:
-//            redColorCircle.alpha = 0.2
-//            redColorCircle.alpha = 0.2
-//            redColorCircle.alpha = 0.2
-//        }
-//    }
-    
 }
 
 
